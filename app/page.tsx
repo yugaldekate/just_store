0196ddc95f68685dbac18b608fea8727d521386d
Yugal D Dekate <yugaldekate72@gmail.com>
2024-11-23 18:14:03 +0530

    setup the project with eslint, tailwind, and globals.css

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl text-brand">
+        Just store - The only storage solution you need
+      </h1>
+    </div>
+  );
+}
